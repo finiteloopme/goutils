bfe1f32b0275905b537e2b7d01cd3922eeda5de4
Kunal Limaye <kunall@google.com>
2022-03-30 16:28:23 +1100

    Adding func ReadEnvVarOptional

--- pkg/os/env.go
+++ pkg/os/env.go
@@ -3,13 +3,18 @@ package pkg
 import (
 	"errors"
 	"os"
+
 	"github.com/finiteloopme/goutils/pkg/log"
 )
 
-func ReadEnvVar(key string) string{
+func ReadEnvVar(key string) string {
 	value := os.Getenv(key)
 	if value == "" {
 		log.Fatal(errors.New("Environment variable " + key + " not set"))
 	}
 	return value
+}
+
+func ReadEnvVarOptional(key string) string {
+	return os.Getenv(key)
 }
