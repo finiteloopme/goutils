b12b7326c902fdf587513219df52ae6ae7b9eec1
Kunal Limaye <kunall@google.com>
2022-03-31 09:20:11 +1100

    Add GCP specific package

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/finiteloopme/goutils
 
 go 1.17
 
-require github.com/sirupsen/logrus v1.8.1
+require (
+	cloud.google.com/go/compute v1.5.0
+	github.com/sirupsen/logrus v1.8.1
+)
 
-require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+require golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
---
+++ pkg/gcp/runtime.go
@@ -0,0 +1,44 @@
+package gcp
+
+import (
+	"net/http"
+
+	"cloud.google.com/go/compute/metadata"
+	"github.com/finiteloopme/goutils/pkg/log"
+)
+
+// Check if the runtime platform is GCP
+// Return true if GCP
+func IsRuntimeGCP() bool {
+	p := GetPRojectID()
+	if p != "" {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Get the GCP project ID
+func GetPRojectID() string {
+	c := metadata.NewClient(&http.Client{Transport: userAgentTransport{
+		userAgent: "my-user-agent",
+		base:      http.DefaultTransport,
+	}})
+	p, err := c.ProjectID()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return p
+}
+
+// userAgentTransport sets the User-Agent header before calling base.
+type userAgentTransport struct {
+	userAgent string
+	base      http.RoundTripper
+}
+
+// RoundTrip implements the http.RoundTripper interface.
+func (t userAgentTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("User-Agent", t.userAgent)
+	return t.base.RoundTrip(req)
+}
