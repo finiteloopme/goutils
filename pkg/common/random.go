51fbbf98acc7631ad88faf8a08277e4e6dfb2dab
Kunal Limaye <kunall@google.com>
2022-08-05 11:45:01 +1000

    Adding a random float func

--- pkg/common/random.go
+++ pkg/common/random.go
@@ -18,3 +18,17 @@ func RandomInt(min, max int) int {
 	ir := intRange{min, max}
 	return ir.randomInt()
 }
+
+type floatRange struct {
+	min, max float64
+}
+
+func (fr floatRange) randomFloat() float64 {
+	rand.Seed(time.Now().UnixNano())
+	return fr.min + (rand.Float64() * (fr.max + fr.min))
+}
+
+func RandomFloat(min, max float64) float64 {
+	fr := floatRange{min, max}
+	return fr.randomFloat()
+}
