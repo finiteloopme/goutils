358b545e0fe6b978395f96c63569396bba387e97
Kunal Limaye <kunall@google.com>
2022-08-14 17:32:09 +1000

    Added CLI support to create golang cloud run app

--- cmd/main.go
+++ cmd/main.go
@@ -35,17 +35,21 @@ func printUsage() {
 }
 
 func ProcessCreateApp() {
-	// Example
-	// goutils create-app --type go-simple --fqdn-name github.com/finiteloopme/demo/go-simple --output go-simple
 	createAppCmd := flag.NewFlagSet(CREATE_APP, flag.ExitOnError)
-	appType := createAppCmd.String("type", "go-simple", "Application type.  Only 'go-simple' is supported")
+	appType := createAppCmd.String("type", "go-simple", "Application type.  Only 'go-simple' or 'go-cloudrun' is supported")
 	appName := createAppCmd.String("name", "", "Application name")
 	fqdnName := createAppCmd.String("fqdn-name", "", "Fully qualified module name to use with `go mod init`")
 	appLocation := createAppCmd.String("output", *appName, "Folder name to host the app")
 	createAppCmd.Parse((os.Args[2:]))
 	switch *appType {
 	case "go-simple":
+		// Example
+		// goutils create-app --name simple-app --type go-simple --fqdn-name github.com/finiteloopme/demo/simple-app --output simple-app
 		codegen.NewSimpleGoModule(*appName, *fqdnName, *appLocation)
+	case "go-cloudrun":
+		// Example
+		// goutils create-app --name cloudrun-app --type go-cloudrun --fqdn-name github.com/finiteloopme/demo/cloudrun-app --output cloudrun-app
+		codegen.NewCloudRunGoModule(*appName, *fqdnName, *appLocation)
 	default:
 		printUsage()
 		os.Exit(1)
