9364a0fc346f553eb16e5fd6446314811997253d
Kunal L <kunallimaye@gmail.com>
2024-07-11 15:10:06 +1000

    Supporting ENV variable for GCP Project ID

--- pkg/gcp/runtime.go
+++ pkg/gcp/runtime.go
@@ -1,11 +1,12 @@
 package gcp
 
 import (
-	"io/ioutil"
+	"os"
 	"runtime"
 	"strings"
 
 	"cloud.google.com/go/compute/metadata"
+	"github.com/finiteloopme/goutils/pkg/log"
 )
 
 // Check if the runtime platform is GCP
@@ -15,7 +16,7 @@ func IsRuntimeGCP() bool {
 		return false
 	}
 
-	_product_name, _ := ioutil.ReadFile("/sys/class/dmi/id/product_name")
+	_product_name, _ := os.ReadFile("/sys/class/dmi/id/product_name")
 	product_name := strings.TrimSpace(string(_product_name))
 	if strings.Contains(product_name, "Google") {
 		return true
@@ -25,32 +26,25 @@ func IsRuntimeGCP() bool {
 }
 
 // Get the GCP project ID
-func GetProjectID() string {
-	// c := metadata.NewClient(&http.Client{
-	// 	Transport: userAgentTransport{
-	// 		userAgent: "kl-gcp-user-agent",
-	// 		base:      http.DefaultTransport,
-	// 	},
-	// 	Timeout: 1000000000, // 1 sec timeout
-	// })
-	// p, err := c.ProjectID()
-
+// If the runtime is not GCP, then specify an optional
+// environment variable to read the project ID from
+// 'GCP_PROJECT' is the default environment variable used
+func GetProjectID(env ...string) string {
+	projectID := ""
 	if IsRuntimeGCP() {
-		p, _ := metadata.ProjectID()
-		return p
+		// Runtime is GCP
+		var err error
+		projectID, err = metadata.ProjectID()
+		if err != nil {
+			log.Warn("Unexpected error reading ProjectID from metadata server. ", err)
+		}
 	} else {
-		return ""
+		env_variable := "GCP_PROJECT"
+		if len(env) > 0 {
+			env_variable = env[0]
+		}
+		projectID = os.Getenv(env_variable)
 	}
+
+	return projectID
 }
-
-// // userAgentTransport sets the User-Agent header before calling base.
-// type userAgentTransport struct {
-// 	userAgent string
-// 	base      http.RoundTripper
-// }
-
-// // RoundTrip implements the http.RoundTripper interface.
-// func (t userAgentTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-// 	req.Header.Set("User-Agent", t.userAgent)
-// 	return t.base.RoundTrip(req)
-// }
